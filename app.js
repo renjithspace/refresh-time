94e95911bc622f66d6baf81155b5b2b5228ec01d
renjithspace <renjithrajeevvk@gmail.com>
2021-10-11 08:20:11 +0530

    Run in minutes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const applescript = require('applescript')
 const cron = require('node-cron')
 
 const applescriptPath = path.resolve('./notify.applescript')
-const expression = `25,55 * * * * *`
+const expression = `25,55 * * * *`
 cron.schedule(expression, () => {
   applescript.execFile(applescriptPath)
 })
